94b7fe84df7caeac15400f39e7836ce1aecdb442
Whitebone <47801148+White-bone@users.noreply.github.com>
2022-05-15 11:12:38 -0400

    Update Netifly-Notepad.js

--- Bookmarklets/Netifly-Notepad.js
+++ Bookmarklets/Netifly-Notepad.js
@@ -1,1 +1,1 @@
-javascript:(t=>{var s=document.createElement(t);s.type='text/java'+t;s.src='https://62811646d59750440564f71b--musical-profiterole-131087.netlify.app/';document.body.appendChild(s);})('script')
+javascript:(t=>{var s=document.createElement(t);s.type='text/java'+t;s.src='https://white-bone.github.io/test/javascript/Netifly-Notepad.js';document.body.appendChild(s);})('script')
