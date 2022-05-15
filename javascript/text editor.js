f433b027bf2a2eb9718e778f7281d904b2448e84
Whitebone <47801148+White-bone@users.noreply.github.com>
2022-05-15 09:15:04 -0400

    Create text editor.js

---
+++ javascript/text editor.js
@@ -0,0 +1,1 @@
+data:text/html, <html contenteditable><html contenteditable="">%0A<head>%0A    <link rel="stylesheet" href="http://yegor256.github.io/tacit/tacit.min.css">%0A</head>%0A<body>%0A</body>%0A</html>
