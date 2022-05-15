be05ed53c523fce2a0ede87767ab62111f1e63ee
Whitebone <47801148+White-bone@users.noreply.github.com>
2022-05-15 09:20:06 -0400

    Update and rename javascript/text-editor-bookmarklet.js to javascriptandbookmarklets/text-editor-bookmarklet.js

--- javascript/text-editor-bookmarklet.js
+++
@@ -1,1 +0,0 @@
-data:text/html, <html contenteditable><html contenteditable="">%0A<head>%0A    <link rel="stylesheet" href="http://yegor256.github.io/tacit/tacit.min.css">%0A</head>%0A<body>%0A</body>%0A</html>
